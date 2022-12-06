fdf9d3d3ff548047c306570b3f4d0f849b4282ca
rfranett <rfranett@gmail.com>
2022-12-05 22:45:40 -0300

    frase do dia

---
+++ .vscode/ex002.js
@@ -0,0 +1,15 @@
+
+/* Desafio 02 */
+const dia = 5
+const mes = 12
+const ano = 2022
+const FraseDoDia = "Seja curioso. Leia de tudo. Tente coisas novas. O que as pessoas chamam de inteligência se resume a curiosidade."
+const Autoria = "Aaron Swartz"
+
+document.write(`<div class="citacao">
+                <h2>Frase do Dia</h2>
+                <h3>${dia}/${mes}/${ano}</h3>
+                <blockquote>&#10077;${FraseDoDia}&#10078;</blockquote>
+                <p>${Autoria}</p>
+                </div>
+               `)
